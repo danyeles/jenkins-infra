c83313f62dd0ce664a87643bb2eed584d6f80bde
danyeles <daniel.espinosa.silva@gmail.com>
2026-02-27 09:45:16 -0600

    Refactor pipeline structure in seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -1,31 +1,25 @@
-pipeline {
-    agent any
+node {
+    stage('Generate Jobs') {
 
-    stages {
-        stage('Generate Jobs') {
-            steps {
-                script {
-                    def apps = readYaml file: 'apps.yaml'
+        def apps = readYaml file: 'apps.yaml'
 
-                    apps.apps.each { app ->
-                        jobDsl scriptText: """
-                            pipelineJob("deploy-${app.name}") {
-                                definition {
-                                    cps {
-                                        script(\"\"\"
-                                            @Library('my-shared-lib') _
-                                            appPipeline(
-                                                appName: '${app.name}',
-                                                configFile: '${app.config}'
-                                            )
-                                        \"\"\".stripIndent())
-                                    }
-                                }
-                            }
-                        """
+        apps.apps.each { app ->
+
+            jobDsl scriptText: """
+                pipelineJob("deploy-${app.name}") {
+                    definition {
+                        cps {
+                            script(\"\"\"
+                                @Library('my-shared-lib') _
+                                appPipeline(
+                                    appName: '${app.name}',
+                                    configFile: '${app.config}'
+                                )
+                            \"\"\".stripIndent())
+                        }
                     }
                 }
-            }
+            """
         }
     }
 }
